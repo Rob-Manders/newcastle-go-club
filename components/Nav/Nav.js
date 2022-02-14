857c314edf8e7041ab529a6175766648ea45a430
Robert Manders <31056731+Rob-Manders@users.noreply.github.com>
2022-02-14 20:58:35 +0000

    Setup project. Begin nav component.

---
+++ components/Nav/Nav.js
@@ -0,0 +1,49 @@
+
+import { useState } from 'react'
+import styles from './Nav.module.scss'
+import Link from 'next/link'
+import Portal from '../../HOC/Portal/Portal'
+
+export default function Nav() {
+	const [menuToggle, setMenuToggle] = useState(false)
+
+	function toggleMenu() {
+		setMenuToggle(prevState => {
+			setMenuToggle(!prevState)
+		})
+	}
+
+	function Links() {
+		return (
+			<>
+				<Link className={styles.navLink} href="/">Home</Link>
+				<Link className={styles.navLink} href="/meetings">Meetings</Link>
+			</>
+		)
+	}
+
+	return (
+		<nav className={styles.nav}>
+			<Link href="/">
+				<img className={styles.logo} src="/NGC-Logo.svg" alt="logo"></img>
+			</Link>
+
+			<div className={styles.desktopLinks}>
+				<Links />
+			</div>
+
+			<div className={styles.mobileMenuButton} onClick={toggleMenu}>
+				{/* TODO: Hamburger icon. */}
+			</div>
+
+			{
+				menuToggle
+				&& 
+				<Portal>
+					<Links />
+				</Portal>
+			}
+			
+		</nav>
+	)
+}
