a6a51c958374d46a653b4b3af29bacd19844bc90
Robert Manders <31056731+Rob-Manders@users.noreply.github.com>
2022-02-16 22:32:17 +0000

    Tweaks before bed...

--- components/NextMeeting/NextMeeting.js
+++ components/NextMeeting/NextMeeting.js
@@ -6,10 +6,10 @@ export default function NextMeeting() {
 		<section className={styles.nextMeeting}>
 			<h2>Next Meeting</h2>
 
-			<p className={styles.date}>Thursday 17th February</p>
 			<p className={styles.time}>19:00</p>
+			<p className={styles.date}>Thursday 17th February</p>
 			<p className={styles.venue}>The Millstone, Gosforth</p>
-			<p className={styles.postCode}>NE3 1QL</p>
+			<p className={styles.postcode}>NE3 1QL</p>
 
 			<Link href='/schedule'>View Schedule</Link>
 		</section>
