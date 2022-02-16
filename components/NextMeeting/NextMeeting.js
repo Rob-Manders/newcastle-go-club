3dd5fb359f201fd0c68568fe92400f96d80fa6c7
Robert Manders <31056731+Rob-Manders@users.noreply.github.com>
2022-02-16 22:01:52 +0000

    Next meeting component.

--- components/NextMeeting/NextMeeting.js
+++ components/NextMeeting/NextMeeting.js
@@ -1,10 +1,17 @@
 
-import styles from './NextMeeting.modules.scss'
-
+import Link from 'next/link'
+import styles from './NextMeeting.module.scss'
 export default function NextMeeting() {
 	return (
 		<section className={styles.nextMeeting}>
-			
+			<h2>Next Meeting</h2>
+
+			<p className={styles.date}>Thursday 17th February</p>
+			<p className={styles.time}>19:00</p>
+			<p className={styles.venue}>The Millstone, Gosforth</p>
+			<p className={styles.postCode}>NE3 1QL</p>
+
+			<Link href='/schedule'>View Schedule</Link>
 		</section>
 	)
 }
--- pages/schedule.js
+++ pages/schedule.js
@@ -3,19 +3,16 @@ import styles from '../styles/Home.module.scss'
 import DocumentHead from '../components/DocumentHead/DocumentHead'
 import Nav from '../components/Nav/Nav'
 import Footer from '../components/Footer/Footer'
-import NextMeeting from '../components/NextMeeting/NextMeeting'
 
-export default function Home() {
+export default function Schedule() {
   return (
     <div className={styles.container}>
-      <DocumentHead pageTitle='Home' overrideTitle/>
+      <DocumentHead pageTitle='Schedule' overrideTitle/>
 
       <Nav page='home'/>
 
-
       <main className={styles.main}>
-        <img className={styles.mainImage} src='images/goclub1.jpg' />
-        <NextMeeting />
+        
       </main>
 
       <Footer />
