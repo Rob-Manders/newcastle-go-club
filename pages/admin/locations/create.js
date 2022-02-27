e6635399d8f480b934f0582e03ac5b4148072978
Robert Manders <31056731+Rob-Manders@users.noreply.github.com>
2022-02-27 20:34:55 +0000

    Locations tweaks.

--- pages/admin/locations/create.js
+++ pages/admin/locations/create.js
@@ -1,11 +1,11 @@
 
 import { useContext } from 'react'
-import Link from 'next/link'
 import styles from '../../../styles/admin/locations/Locations.module.scss'
 import { AuthContext } from '../../../context/AuthContext'
 import DocumentHead from '../../../components/DocumentHead/DocumentHead'
 import DefaultLayout from '../../../layouts/DefaultLayout'
 import LocationForm from '../../../components/LocationForm/LocationForm'
+import AdminMenu from '../../../components/AdminMenu/AdminMenu'
 
 export default function CreateLocation() {
 	const { loggedIn, accountType } = useContext(AuthContext)
@@ -14,7 +14,13 @@ export default function CreateLocation() {
 		<div className={styles.container}>
 			<DocumentHead pageTitle='Admin - Locations' noIndex />
 			<DefaultLayout>
-				{loggedIn && accountType === 'admin' && <LocationForm />}
+				<AdminMenu />
+				{loggedIn && accountType === 'admin' && 
+					<div className={styles.createLocation}>
+						<h2>Add Location</h2>
+						<LocationForm />
+					</div>
+				}
 			</DefaultLayout>
 		</div>
 	)
