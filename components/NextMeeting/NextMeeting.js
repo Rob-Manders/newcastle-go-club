f2657dd220b137cd4b5aca263f2e07cffc87b312
Robert Manders <31056731+Rob-Manders@users.noreply.github.com>
2022-04-11 11:43:22 +0100

    Next meeting logic.

--- components/NextMeeting/NextMeeting.js
+++ components/NextMeeting/NextMeeting.js
@@ -1,27 +1,68 @@
 
-import Link from 'next/link'
+import { useState, useEffect } from 'react'
 import styles from './NextMeeting.module.scss'
+import useParseDate from '../../hooks/useParseDate'
 import Map from '../Map/Map'
 import Button from '../../components/Button/Button'
 
-export default function NextMeeting() {
+export default function NextMeeting({ meetings, locations }) {
+	const parseDate = useParseDate()
+	const [meeting, setMeeting] = useState({})
+	const [location, setLocation] = useState({
+		name: 'TBC',
+		area: 'Newcastle upon Tyne',
+		postcode: 'TBC',
+		cpsCoord: '54.98226216073709,-1.6170193570421796'
+	})
+	
+	function findLocation(locationId) {
+		locations.forEach(location => {
+			if (location._id === locationId) {
+				setLocation(location)
+			}
+		})
+	}
+
+	function findNextMeeting() {
+		meetings.forEach(meeting => {
+			meeting.dateObject = new Date(`${meeting.date} ${meeting.time}`)
+		})
+
+		let nextMeeting = meetings[0]
+
+		for (let i = 1; i < meetings.length; i++) {
+			if (meetings[i].dateObject < nextMeeting.dateObject) {
+				nextMeeting = meetings[i]
+			}
+		}
+
+		setMeeting(nextMeeting)
+	}
+
+	useEffect(() => {
+		findNextMeeting()
+	})
+
+	useEffect(() => {
+		findLocation(meeting.locationId)
+	}, [meeting])
+
 	return (
 		<section className={styles.nextMeeting}>
 			<div className={styles.container}>
 				<h2>Next Meeting</h2>
 
-				<p className={styles.time}>19:00</p>
-				<p className={styles.date}>Thursday 17th February</p>
-				<p className={styles.venue}>The Millstone, Gosforth</p>
-				<p className={styles.postcode}>NE3 1QL</p>
+				<p className={styles.time}>{meeting.time}</p>
+				<p className={styles.date}>{parseDate(meeting.date)}</p>
+				<p className={styles.venue}>{location.name}, {location.area}</p>
+				<p className={styles.postcode}>{location.postcode}</p>
 
 			</div>
 
 			<div className={styles.mapContainer}>
-				<Map mapsApiKey='AIzaSyDELpcbkMle2OUpJwR-vKKLFWh4NRP9Fro' gpsCoord='55.00497071291975,-1.6030312609801245'/>
+				<Map mapsApiKey='AIzaSyDELpcbkMle2OUpJwR-vKKLFWh4NRP9Fro' gpsCoord={location.gpsCoord}/>
 			</div>
 			
-			{/* <Link href='/schedule'>View Schedule</Link> */}
 			<Button href='/meetings'>View Schedule</Button>
 		</section>
 	)
--- pages/index.js
+++ pages/index.js
@@ -4,8 +4,11 @@ import Image from 'next/image'
 import DocumentHead from '../components/DocumentHead/DocumentHead'
 import DefaultLayout from '../layouts/DefaultLayout'
 import NextMeeting from '../components/NextMeeting/NextMeeting'
+import dbConnect from '../lib/dbConnect'
+import MeetingModel from '../models/MeetingModel'
+import LocationModel from '../models/LocationModel'
 
-export default function Home() {
+export default function Home({ meetings, locations }) {
   return (
     <div className={styles.container}>
       <DocumentHead pageTitle='Home' />
@@ -14,9 +17,29 @@ export default function Home() {
         <div className={styles.mainImage}>
           <Image src='/images/goclub1.jpg' layout='fill' objectFit='cover' priority />
         </div>
-        <NextMeeting />
+        <NextMeeting meetings={meetings} locations={locations} />
       </DefaultLayout>
 
     </div>
   )
+}
+
+export async function getServerSideProps() {
+	await dbConnect()
+
+	const meetingsResult = await MeetingModel.find({}, 'date time locationId')
+	const meetings = meetingsResult.map(document => {
+		const meeting = document.toObject()
+		meeting._id = meeting._id.toString()
+		return meeting
+	})
+
+	const locationsResult = await LocationModel.find({})
+	const locations = locationsResult.map(document => {
+		const location = document.toObject()
+		location._id = location._id.toString()
+		return location
+	})
+
+	return { props: { meetings, locations } }
 }
